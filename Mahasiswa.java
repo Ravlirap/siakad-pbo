53af054fbb6b3e20761f24143403267dedaa917d
Ravlirap <ravli190606@gmail.com>
2025-09-20 21:44:51 +0700

    kode program
    tugas pbo

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -1,3 +1,20 @@
 public class Mahasiswa {
-    
+    public String nama;
+    public String nim;
+    public String jurusan;
+    public int umur;
+
+    public Mahasiswa(String nama, String nim, String jurusan, int umur){
+        this.nama = nama;
+        this.nim = nim;
+        this.jurusan = jurusan;
+        this.umur = umur;
+    }
+
+    public void showMahasiswa(){
+        System.out.println("Nama Mahasiswa : " + this.nama);
+        System.out.println("NIM Mahasiswa : " + this.nim);
+        System.out.println("Jurusan Mahasiswa : " + this.jurusan);
+        System.out.println("Umur Mahasiswa : " + this.umur + " tahun");
+    }
 }
--- MahasiswaTest.java
+++ MahasiswaTest.java
@@ -1,3 +1,11 @@
 public class MahasiswaTest {
     
+    public static void main(String[] args) {
+        Mahasiswa mhs1 = new Mahasiswa("Ravli", "2403066", "Teknik Informatika", 19);
+        Mahasiswa mhs2 = new Mahasiswa("Karin", "2403043", "Teknik Informatika", 18);
+        Mahasiswa mhs3 = new Mahasiswa("Azhar", "2403063", "Teknik Informatika", 20);
+        mhs1.showMahasiswa();
+        mhs2.showMahasiswa();
+        mhs3.showMahasiswa();
+    }
 }
