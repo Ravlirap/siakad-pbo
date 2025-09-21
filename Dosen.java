f7a417dc51ff3522e518214f2cf8eda0be7b41e0
Azhar0121 <azharathallah338@gmail.com>
2025-09-21 13:39:30 +0700

    Data Dosen

--- Dosen.java
+++ Dosen.java
@@ -1,3 +1,18 @@
 public class Dosen{
     
+    public String nama;
+    public String nip;
+    public String fungsional;
+
+    public Dosen(String nama, String nip ,String fungsional){
+        this.nama = nama;
+        this.nip = nip;
+        this.fungsional = fungsional;
+    }
+
+    public void showDosen(){
+        System.out.println("Nama Dosen       : " + this.nama);
+        System.out.println("NIP Dosen        : " + this.nip);
+        System.out.println("Fungsional Dosen : " + this.fungsional);
+    }
 }
--- DosenTest.java
+++ DosenTest.java
@@ -1,3 +1,13 @@
 public class DosenTest {
     
+    public static void main(String[] args) {
+        Dosen dosen1 = new Dosen ("Fachrul Pralienka Bani Muhamad, S.ST., M.Kom", "199204232018031001", "Asisten Ahli");
+        dosen1.showDosen();
+
+        Dosen dosen2 = new Dosen ("Muhamad Mustamiin, S.Pd.,M.Kom", "199205052019031011", "Lektor");
+        dosen2.showDosen();
+
+        Dosen dosen3 = new Dosen ("Nur Budi Nugraha, S.Kom., MT", "198711162022031001", "Lektor");
+        dosen3.showDosen();
+    }
 }
