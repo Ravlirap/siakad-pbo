65ff7ac9f897a81f26d9f2f2f96fc4024eea539d
karinana21 <karinasiti2101@gmail.com>
2025-09-22 09:57:32 +0700

    siti karinah(24030480

--- MataKuliah.java
+++ MataKuliah.java
@@ -1,12 +1,12 @@
 public class MataKuliah {
-
     public String kodeMatkul;
     public String namaMatkul;
     public int SKS;
     public String namaKelas;
     public String jadwal;
 
-    public MataKuliah(String kodeMatkul, String namaMatkul, int SKS, String namaKelas, String jadwal){
+    // Constructor
+    public MataKuliah(String kodeMatkul, String namaMatkul, int SKS, String namaKelas, String jadwal) {
         this.kodeMatkul = kodeMatkul;
         this.namaMatkul = namaMatkul;
         this.SKS = SKS;
@@ -14,6 +14,7 @@ public class MataKuliah {
         this.jadwal = jadwal;
     }
 
+    // Method tampilkan semua atribut
     public void showMataKuliah() {
         System.out.println("===== Data Mata Kuliah =====");
         System.out.println("Kode Mata Kuliah : " + this.kodeMatkul);
@@ -21,5 +22,31 @@ public class MataKuliah {
         System.out.println("Jumlah SKS       : " + this.SKS);
         System.out.println("Nama Kelas       : " + this.namaKelas);
         System.out.println("Jadwal Kuliah    : " + this.jadwal);
+    }
+
+    // Method tampilkan berdasarkan pilihan
+    public void show(String pilihan) {
+        switch (pilihan.toLowerCase()) {
+            case "kode":
+                System.out.println("Kode Mata Kuliah : " + this.kodeMatkul);
+                break;
+            case "nama":
+                System.out.println("Nama Mata Kuliah : " + this.namaMatkul);
+                break;
+            case "sks":
+                System.out.println("Jumlah SKS       : " + this.SKS);
+                break;
+            case "kelas":
+                System.out.println("Nama Kelas       : " + this.namaKelas);
+                break;
+            case "jadwal":
+                System.out.println("Jadwal Kuliah    : " + this.jadwal);
+                break;
+            case "all":
+                this.showMataKuliah();
+                break;
+            default:
+                System.out.println("Pilihan tidak valid. Gunakan: kode, nama, SKS, kelas, jadwal, all");
+        }
     }
 }
--- MataKuliahTest.java
+++ MataKuliahTest.java
@@ -1,22 +1,35 @@
+import java.util.Scanner;
+
 public class MataKuliahTest {
-
     public static void main(String[] args) {
-        MataKuliah mk1 = new MataKuliah("TIP2301", "Bahasa Inggris", 2, "22", "Selasa, 10:50 s.d 14:50 @Ruang Kuliah A");
-        MataKuliah mk2 = new MataKuliah("TIP4301", "Internet Of Things", 3, "22", "Selasa 10.00 s.d 13.30 @LAB.IOT, Jumat 13.50 s.d 15:30 @Ruang 605");
-        MataKuliah mk3 = new MataKuliah("TIP4302", "Metode Numerik", 3, "22", "Selasa, 07:30 s.d 10:50 @Ruang 409 ");
-        MataKuliah mk4 = new MataKuliah("TIU2301", "Proyek 2", 3, "22", "Rabu 14:20 s.d 16:00 @Ruang 502, Jumat 07:30 s.d 10:50 @LAB.IOT");
-        MataKuliah mk5 = new MataKuliah("TIU3301", "Adiministrasi Sistem dan Jaringan Komputer", 3, "22", "Rabu 07:30 s.d 10:00 @LAB.JARKOM, Kamis 07:30 s.d 09:10 @Ruang 604");
-        MataKuliah mk6 = new MataKuliah("TIU3302", "Pemrograman Berorientasi Objek", 3, "22", "Senin 10:00 s.d 13:30 @LAB.PEMROGRAMAN, Selasa 14:20 s.d 16:00 @Ruang 501");
-        MataKuliah mk7 = new MataKuliah("TIU3303", "Pemrograman Web Lanjut", 3, "22", "Senin 07:30 s.d 09:10, Rabu 10:00 s.d 13:30 @LAB.PEMROGRAMAN");
+        MataKuliah[] daftarMK = {
+            new MataKuliah("TIP2301", "Bahasa Inggris", 2, "22", "Selasa, 10:50 s.d 14:50 @Ruang Kuliah A"),
+            new MataKuliah("TIP4301", "Internet Of Things", 3, "22", "Selasa 10.00 s.d 13.30 @LAB.IOT, Jumat 13.50 s.d 15:30 @Ruang 605"),
+            new MataKuliah("TIP4302", "Metode Numerik", 3, "22", "Selasa, 07:30 s.d 10:50 @Ruang 409"),
+            new MataKuliah("TIU2301", "Proyek 2", 3, "22", "Rabu 14:20 s.d 16:00 @Ruang 502, Jumat 07:30 s.d 10:50 @LAB.IOT"),
+            new MataKuliah("TIU3301", "Administrasi Sistem dan Jaringan Komputer", 3, "22", "Rabu 07:30 s.d 10:00 @LAB.JARKOM, Kamis 07:30 s.d 09:10 @Ruang 604"),
+            new MataKuliah("TIU3302", "Pemrograman Berorientasi Objek", 3, "22", "Senin 10:00 s.d 13:30 @LAB.PEMROGRAMAN, Selasa 14:20 s.d 16:00 @Ruang 501"),
+            new MataKuliah("TIU3303", "Pemrograman Web Lanjut", 3, "22", "Senin 07:30 s.d 09:10, Rabu 10:00 s.d 13:30 @LAB.PEMROGRAMAN")
+        };
 
-        // Menampilkan semua atribut sekaligus
-        mk1.showMataKuliah();
-        mk2.showMataKuliah();
-        mk3.showMataKuliah();
-        mk4.showMataKuliah();
-        mk5.showMataKuliah();
-        mk6.showMataKuliah();
-        mk7.showMataKuliah();
+        Scanner input = new Scanner(System.in);
+
+        // Pilih mata kuliah
+        System.out.print("Pilih nomor mata kuliah (1-7): ");
+        int pilihan = input.nextInt();
+        input.nextLine(); // clear buffer enter
+
+        if (pilihan >= 1 && pilihan <= daftarMK.length) {
+            // Pilih atribut apa yang mau ditampilkan
+            System.out.print("Mau tampilkan apa? (kode/nama/sks/kelas/jadwal/all): ");
+            String atribut = input.nextLine();
+
+            // Panggil method show dengan parameter
+            daftarMK[pilihan - 1].show(atribut);
+        } else {
+            System.out.println("Pilihan tidak valid.");
+        }
+
+        input.close();
     }
-    
 }
